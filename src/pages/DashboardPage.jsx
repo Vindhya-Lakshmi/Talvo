85dfbc489f4d8196ba268d8a2a69445cb34001e4
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-01-26 12:21:20 +0530

    homepage completed

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      xrdctfgbhnjkm
+    </div>
+  )
+}
+
+export default DashboardPage
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router"
-import {ArrowRightIcon, CheckIcon, SparklesIcon, VideoIcon, ZapIcon} from "lucide-react"
-import {SignInButton} from "@clerk/clerk-react"
+import { ArrowRightIcon, CheckIcon, Code2Icon, SparklesIcon, UsersIcon, VideoIcon, ZapIcon } from "lucide-react"
+import { SignInButton } from "@clerk/clerk-react"
 
 
 function HomePage() {
@@ -8,41 +8,41 @@ function HomePage() {
     <div className="bg-gradient-to-br from-base-100 via-base-200 to-base-300">
       {/* NAVBAR */}
       <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top z-50 shadow-lg">
-      <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
-        {/* LOGO */}
-        <Link to = {"/"}
-        className="flex items-center gap-3 hover:scale-105 transition-transform duration-200">
-          <div className="size-10 rounded-xl bg-gradient-to-br from-primary via-secondary to-accent 
+        <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
+          {/* LOGO */}
+          <Link to={"/"}
+            className="flex items-center gap-3 hover:scale-105 transition-transform duration-200">
+            <div className="size-10 rounded-xl bg-gradient-to-br from-primary via-secondary to-accent 
           flex items-center justify-center shawdow-lg">
-            <SparklesIcon className="size-6 text-white"/>
-          </div>
-          <div className="flex flex-col">
-            <span className="font-black text-xl bg-gradient-to-r from-primary via-secondary 
-            to-accent bg-clip-text text-transparent font-mono tracking-wider"> Talent IQ</span> 
-            <span className="text-xs text-base-content/60 font-medium -ml-1"> Code Together</span>
-          </div>
+              <SparklesIcon className="size-6 text-white" />
+            </div>
+            <div className="flex flex-col">
+              <span className="font-black text-xl bg-gradient-to-r from-primary via-secondary 
+            to-accent bg-clip-text text-transparent font-mono tracking-wider"> Talent IQ</span>
+              <span className="text-xs text-base-content/60 font-medium -ml-1"> Code Together</span>
+            </div>
 
-        </Link>
+          </Link>
 
-        {/* Auth btn */}
-        <SignInButton mode="modal">
-          <button className="group px-6 py-3 bg-gradient-to-r from-primary to-secondary rounded-xl 
+          {/* Auth btn */}
+          <SignInButton mode="modal">
+            <button className="group px-6 py-3 bg-gradient-to-r from-primary to-secondary rounded-xl 
           text-white font-semibold text-sm shadow-lg hover:shadow-xl transition-all
           duration-200 hover:scale-105 flex items-center gap-2">
-            <span>Get Started</span>
-            <ArrowRightIcon className="size-4 group-hover:translate-x-0.5"/>
-          </button>
-        </SignInButton>
-        
-      </div>
+              <span>Get Started</span>
+              <ArrowRightIcon className="size-4 group-hover:translate-x-0.5" />
+            </button>
+          </SignInButton>
+
+        </div>
       </nav>
       {/* HERO SECTION */}
       <div className="max-w-7xl mx-auto px-4 py-20">
         <div className="grid lg:grid-cols-2 gap-12 items-center">
-           {/* LEFT CONTENT */}
-           <div className="space-y-8">
+          {/* LEFT CONTENT */}
+          <div className="space-y-8">
             <div className="badge badge-primary badge-lg">
-              <ZapIcon className="size-4"/>
+              <ZapIcon className="size-4" />
               Real-time collaboration
             </div>
             <h1 className="text-5xl lg:text-7xl font-black leading-tight">
@@ -52,21 +52,21 @@ function HomePage() {
               <span className="text-base-content">Learn Together</span>
             </h1>
             <p className="text-xl text-base-content/70 leading-elaxed max-w-xl">
-            The ultimate platform for collaboration coding interviews and pair programming.
-            Connect face-to-face, code in real-time, and ace your technical interview.</p>
-            
+              The ultimate platform for collaboration coding interviews and pair programming.
+              Connect face-to-face, code in real-time, and ace your technical interview.</p>
+
             {/* FEATURE PILLS */}
             <div className="flex flex-wrap gap-3">
               <div className="badge badge-lg badge-outline">
-                <CheckIcon className="size-4 text-success"/>
+                <CheckIcon className="size-4 text-success" />
                 Code Editor
               </div>
               <div className="badge badge-lg badge-outline">
-                <CheckIcon className="size-4 text-success"/>
+                <CheckIcon className="size-4 text-success" />
                 Multi Language
               </div>
               <div className="badge badge-lg badge-outline">
-                <CheckIcon className="size-4 text-success"/>
+                <CheckIcon className="size-4 text-success" />
                 Live video chat
               </div>
             </div>
@@ -76,12 +76,12 @@ function HomePage() {
               <SignInButton mode="modal">
                 <button className="btn btn-primary btn-lg">
                   Start coding now
-                    <ArrowRightIcon className="size-5"/>
+                  <ArrowRightIcon className="size-5" />
                 </button>
               </SignInButton>
 
               <button className="btn btn-outline btn-lg">
-                <VideoIcon className="size-5"/>
+                <VideoIcon className="size-5" />
                 Watch demo
               </button>
             </div>
@@ -101,12 +101,64 @@ function HomePage() {
                 <div className="stat-title">Uptime</div>
               </div>
             </div>
-           </div>
+          </div>
 
-          {/*  */}
+          {/* RIGHT IMAGE */}
+          <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f"
+            alt="Homepage Banner"
+            className="w-full h-auto rounded-3xl shadow-2xl border-4 border-base-100 hover:scale-105 
+          transition-transform duration-500"/>
         </div>
       </div>
-       
+
+      {/* FEATURES SECTION */}
+      <div className="max-w-7xl mx-auto px-4 py-20">
+        <div className="text-center mb-16">
+          <h2 className="text-4xl font-bold mb-4">
+            Everything You Need to <span className="text-primary font-mono">Succeed</span>
+          </h2>
+          <p className="text-lg text-base-content/70 max-w-2xl mx-auto">
+            Powerful features designed to make your coding interviews seamless and productive</p>
+        </div>
+
+        {/* FEATURES GRID */}
+        <div className="grid md:grid-cols-3 gap-8">
+          {/* feature 1 */}
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center 
+              mb-4">
+                <VideoIcon className="size-8 text-primary" />
+              </div>
+              <h3 className="card-title">video call</h3>
+              <p className="text-base-content/70">Crystal clear video and audio for communication during interview</p>
+            </div>
+          </div>
+
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center 
+              mb-4">
+                <Code2Icon className="size-8 text-primary" />
+              </div>
+              <h3 className="card-title">Live code editor</h3>
+              <p className="text-base-content/70">Collaborat in freal-time with syntax highlighting and multiple language support</p>
+            </div>
+          </div>
+
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center 
+              mb-4">
+                <UsersIcon className="size-8 text-primary" />
+              </div>
+              <h3 className="card-title">Easy Collaboration</h3>
+              <p className="text-base-content/70">Share your screen, discuss solutions, and learn from  each other in real-time</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
