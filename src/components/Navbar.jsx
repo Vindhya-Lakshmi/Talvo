4e754d439724d8e50e334a952d124a455013a4ce
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-02-01 14:30:08 +0530

    problems page completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import {Link, useLocation} from "react-router"
-import {SparklesIcon} from "lucide-react"
+import {BookOpenIcon, LayoutDashboardIcon, SparklesIcon} from "lucide-react"
+import {UserButton} from "@clerk/clerk-react"
 
 function Navbar() {
   const location = useLocation()
@@ -30,8 +31,30 @@ function Navbar() {
           {/* problems page link */}
           <Link to={"/problems"}
            className={`px-4 py-2,5 rounded-lg transition-all duration-200 
-            ${isActive("/problems") ? "bg-primary text-primary-content" : "hover:bg-base-200 text-base-content/70 hover:text-base-content"}`}
-           ></Link>
+            ${isActive("/problems") ? "bg-primary text-primary-content" : "hover:bg-base-200 text-base-content/70 hover:text-base-content"}
+            `}
+          
+           >
+            <div className="flex items-center gap-x-2.5">
+              <BookOpenIcon className="size-4"/>
+              <span className="font-medium hidden sm:inline ">Problems</span>
+            </div>
+           </Link>
+
+            <Link to={"/dashboard"}
+           className={`px-4 py-2,5 rounded-lg transition-all duration-200 
+            ${isActive("/dashboard") ? "bg-primary text-primary-content" : "hover:bg-base-200 text-base-content/70 hover:text-base-content"}
+            `}
+          
+           >
+            <div className="flex items-center gap-x-2.5">
+              <LayoutDashboardIcon className="size-4"/>
+              <span className="font-medium hidden sm:inline ">Dashboard</span>
+            </div>
+           </Link>
+           <div className="ml-4 mt-2">
+            <UserButton/>
+           </div>
         </div>
     </div>
   </nav>
