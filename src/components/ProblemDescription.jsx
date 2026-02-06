1fe446a180c2c961f8492b59063106ae6843eab1
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-02-06 20:55:46 +0530

    mjhytedrsdfgh

--- src/components/ProblemDescription.jsx
+++ src/components/ProblemDescription.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import {getDifficultyBadgeClass} from '../lib/utills'
 
-const ProblemDescription = () => {
+function ProblemDescription({problem, currentProblemId, onProblemChange, allProblems})  {
   return (
-    <div>
-      poiuytrewq
+    <div className='h-full overflow-y-auto bg-base-200'>
+      {/* head section */}
+      <div className='p-6 bg-base-100 border-b border-base-300'>
+        <h1 className='text-3xl font-bold text--base-content'>{Problem.title}</h1>
+        <span className={`badge ${getDifficultyBadgeClass(problem.difficulty)}`}>
+          {problem.difficulty}
+        </span>
+      </div>
+        
     </div>
   )
 }
