e7af77c33a017e23f77363d1cf5233a5903b6dd3
unknown <vindhyalakshmiofficial@gmail.com>
2026-02-13 10:48:19 +0530

    new device

--- src/components/OutputPanel.jsx
+++ src/components/OutputPanel.jsx
@@ -2,19 +2,12 @@ import React from 'react'
 
 const OutputPanel = () => {
   return (
-    <div>
-      zxcvbn
-      lihmoifct
-      yhrt
-      yh
-      ty
-      rdty
-      drt
-      ydr
-      ty
-      drty
-      drt
-      y
+    <div className='h-full bg-base-100 flex flex-col'>
+      <div className='px-4 py-2 bg-base-200 border-base-300 font-smeibold text-sm'>
+        Output
+      </div>
+     
+   
     </div>
   )
 }
