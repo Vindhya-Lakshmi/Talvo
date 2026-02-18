206bc802bfbb28e415408f5519ef3fadd0a863b1
unknown <vindhyalakshmiofficial@gmail.com>
2026-02-18 15:58:32 +0530

    correct error

--- src/api/sessions.js
+++ src/api/sessions.js
@@ -3,7 +3,7 @@ import axiosInstance from "../lib/axios"
 
 export const sessionApi = {
     createSession: async (data) => {
-        const response = await axiosInstance.post("/sessions",data)
+        const response = await axiosInstance.get("/sessions",data)
         return response.data
     },
 
